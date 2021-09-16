17e0ea936fcc4fcf395699a95da6efa9c6693840
권태호 <taehokwon48@gmail.com>
2021-09-16 16:07:48 +0900

    testmode

--- server.js
+++ server.js
@@ -5,16 +5,14 @@ const parser = require('xml2json-light');
 const request = require('request');
 const cheerio = require('cheerio');
 const axios = require('axios')
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static(__dirname+"/public"));
+app.set(express.static(__dirname+"/public"));
 
 
 app.get("/",(req,res)=>{
 	res.sendFile(__dirname+"public/index.html");
 });
 
-app.listen(3000,(err)=>{
+app.listen(port,(err)=>{
 	if(err) return console.log(err);
 	console.log("The server is listening on port 3000");
 
